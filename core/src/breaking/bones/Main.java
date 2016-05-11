827828307570fe09c733c603b5fe81d45d0d6420
Dario Woloszin <3h2so4@gmail.com>
2016-05-11 10:51:31 -0300

    first commit

---
+++ core/src/breaking/bones/Main.java
@@ -0,0 +1,30 @@
+package breaking.bones;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Main extends ApplicationAdapter {
+	SpriteBatch batch;
+	public  static final int WIDTH = 800;
+	public  static final int HEIGHT = 600;
+	public static final String TITLE = "Breaking Bones";
+
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+
+		batch.end();
+	}
+}
---
+++ core/src/breaking/bones/States/State.java
@@ -0,0 +1,24 @@
+package breaking.bones.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by wolos on 11/05/2016.
+ */
+public abstract class State {
+    /*protected OrthographicCamera cam;
+    protected Vector3 mouse;
+    protected GameStateMenager gameStateMenager;
+
+    protected State(GameStateMenager gameStateMenager){
+        this.gameStateMenager = gameStateMenager;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
+    protected abstract void handleImput();
+    public abstract void update(float dt);
+    public abstract void render (SpriteBatch sprite);
+    */
+}
